528cc3cc13e49a402319c2306fb19659c665388d
Bookja <gfgfgf20@naver.com>
2022-09-02 10:55:22 +0900

    임홍제

---
+++ src/test/test03.java
@@ -0,0 +1,16 @@
+package test;
+
+public class test03 {
+
+	public test03() {
+		// TODO Auto-generated constructor stub
+	}
+ 
+	public static void main(String[] args) {
+	
+		System.out.println("임홍제");
+		
+	}
+	
+	
+}
